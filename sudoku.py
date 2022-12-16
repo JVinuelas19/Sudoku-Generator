8fa1442945e9fadf9c82138c2606d1917013aed3
JVinuelas19 <JVinuelas19@gmail.com>
2022-12-16 11:56:24 +0100

    Prueba para actualizar el codigo en github

--- sudoku.py
+++ sudoku.py
@@ -1,4 +1,20 @@
 from random import randint
 
 sequence = [1,2,3,4,5,6,7,8,9]
-rows = [], columns = []
+rows = [] 
+columns = []
+blocks = []
+
+for i in range(9):
+    rows.append([]), columns.append([]), blocks.append([])
+
+for row in rows:
+    for column in columns:
+        number = randint(1, 9)
+        column.append(number)
+        row.append(number)
+
+
+for column in columns:
+    print (column)
+

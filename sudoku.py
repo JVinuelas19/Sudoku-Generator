ea786cdc85a0d7baf87e5cff7608762fa9e0016b
juanantonio.vinuelas <jvinuelas19@gmail.com>
2022-12-21 14:41:15 +0100

    The sudoku generator is completed. Now it can generates 4x4 and 6x6 sudokus too.

--- sudoku.py
+++ sudoku.py
@@ -2,32 +2,66 @@ from random import randint
 import time
 import re
 
-def insert_into_block(row, column, blocks):
-    if (row<3):
-        if column<3:
-            return blocks[0]
-        elif column>=3 and column<6:
-            return blocks[1]
+def insert_into_block(row, column, blocks, dimensions):
+    #9x9 block distribution
+    if dimensions == 9:
+        if row<3:
+            if column<3:
+                return blocks[0]
+            elif column>=3 and column<6:
+                return blocks[1]
+            else:
+                return blocks[2]
+        elif row>=3 and row<6:
+            if column<3:
+                return blocks[3]
+            elif column>=3 and column<6:
+                return blocks[4]
+            else:
+                return blocks[5]
         else:
-            return blocks[2]
-    elif row>=3 and row<6:
-        if column<3:
-            return blocks[3]
-        elif column>=3 and column<6:
-            return blocks[4]
-        else:
-            return blocks[5]
-    else:
-        if column<3:
-            return blocks[6]
-        elif column>=3 and column<6:
-            return blocks[7]
-        else:
-            return blocks[8]
+            if column<3:
+                return blocks[6]
+            elif column>=3 and column<6:
+                return blocks[7]
+            else:
+                return blocks[8]
 
-def gen_9x9_sudoku(print_me):
+    #6x6 block_distribution
+    elif dimensions == 6 :
+        if row<2:
+            if column<3:
+                return blocks[0]
+            else:
+                return blocks[1]
+        elif row>=2 and row<4:
+            if column<3:
+                return blocks[2]
+            else:
+                return blocks[3]
+        else:
+            if column<3:
+                return blocks[4]
+            else:
+                return blocks[5]
+    #4x4 block distribution    
+    elif dimensions == 4 :
+        if row<2:
+            if column<2:
+                return blocks[0]
+            if column>=2:
+                return blocks[1]
+        else:
+            if column<2:
+                return blocks[2]
+            if column>=2:
+                return blocks[3]
+    else:
+        print(f'Error in insert_into_block function: No matching dimensions. Value for dimensions is {dimensions} .')
+
+def gen_sudoku(print_me, dimensions):
     rows, columns, blocks = [], [], []
-    for i in range(9):
+    for i in range(dimensions):
         rows.append([]), columns.append([]), blocks.append([])
 
     sudoku_reset = True
@@ -41,10 +75,15 @@ def gen_9x9_sudoku(print_me):
             while row_reset is True:
                 for column in columns:
                     column_index = columns.index(column)
-                    block = insert_into_block(row_index, column_index, blocks)
-                    #Calcular current block de blocks
+                    block = insert_into_block(row_index, column_index, blocks, dimensions)
                     column_reset = True
-                    pool = [1,2,3,4,5,6,7,8,9]
+                    if dimensions == 9:
+                        pool = [1,2,3,4,5,6,7,8,9]
+                    elif dimensions == 6:
+                        pool = [1,2,3,4,5,6]
+                    else:
+                        pool = [1,2,3,4]      
+
                     while(column_reset is True):
                         try:
                             index = randint (0, len(pool)-1)
@@ -61,13 +100,13 @@ def gen_9x9_sudoku(print_me):
                             if time.time()-tic>0.011:
                                 rows.clear(), columns.clear(), blocks.clear()
                                 #After the clear we need to reset the lists, otherwise the algoritm will get stuck
-                                for i in range(9):
+                                for i in range(dimensions):
                                     rows.append([]), columns.append([]), blocks.append([])
                                 regen = True
                             else:
                                 row.clear()
                                 for i in range(column_index):
-                                    block = insert_into_block (row_index, i, blocks)
+                                    block = insert_into_block (row_index, i, blocks, dimensions)
                                     block.pop(-1)
                                     selected_column = columns[i]
                                     selected_column.pop(-1)
@@ -76,10 +115,10 @@ def gen_9x9_sudoku(print_me):
                             #sys.exit()
                     if reset is True:
                         break  
-
+                #Boolean values management
                 if reset is False:
                     row_reset = False
-                    if row_index == 8:
+                    if row_index == dimensions-1:
                         sudoku_reset = False
                 elif reset is True and regen is False:
                     reset = False
@@ -87,51 +126,24 @@ def gen_9x9_sudoku(print_me):
                     break
             if regen is True:
                 break
+
+    toc = time.time()        
     if print_me is True:
+        print(f'El sudoku se ha generado en {round((toc-tic), 4)} segundos y es:')
         for row in rows:
             print(row)
-
-    toc = time.time()
-    #print(f'El sudoku se ha generado en {round((toc-tic), 4)} segundos y es:')
-    #for row in rows:
-    #    print (row)
     return rows
-
-def gen_6x6_sudoku():
-    rows, columns, blocks = [],[],[]
-    for i in range(6):
-        rows.append([]), columns.append([]), blocks.append([])
-
-def gen_4x4_sudoku():
-    rows, columns, blocks = [],[],[]
-    for i in range(4):
-        rows.append([]), columns.append([]), blocks.append([])
 
 def export_sudokus(number, dimensions):
     try:
         tic = time.time()
-        with open ('stuff/sudokus.txt', 'w') as sudokus:
-            if dimensions == 9:
-                for i in range (number):
-                    rows = gen_9x9_sudoku(False)
-                    for row in rows:
-                        sudokus.write(str(row))
-                        sudokus.write(',')
-                    sudokus.write('\n')
-            elif dimensions == 6:
-                for i in range (number):
-                    rows = gen_6x6_sudoku()
-                    for row in rows:
-                        sudokus.write(str(row))
-                        sudokus.write(',')
-                    sudokus.write('\n')
-            elif dimensions == 4:
-                for i in range (number):
-                    rows = gen_4x4_sudoku()
-                    for row in rows:
-                        sudokus.write(str(row))
-                        sudokus.write(',')
-                    sudokus.write('\n')
+        with open (f'stuff/sudokus{dimensions}.txt', 'w') as sudokus:
+            for i in range (number):
+                rows = gen_sudoku(False, dimensions)
+                for row in rows:
+                    sudokus.write(str(row))
+                    sudokus.write(',')
+                sudokus.write('\n')
         toc = time.time()
         print(f'{number} sudokus of {dimensions}x{dimensions} generated in {round(toc-tic , 2)}')
     except:
@@ -176,16 +188,19 @@ def number_of_sudokus(path):
         return lines
 
 def main():
+    NINE_X_NINE = 9
+    SIX_X_SIX = 6
+    FOUR_X_FOUR = 4
     while(True):
         option = input('Welcome to sudoku generator. What do you want to do?:\n1 - Generate a 9x9 sudoku\n2 - Generate a 6x6 sudoku'+
         '\n3 - Generate a 4x4 sudoku\n4 - Export sudokus to a txt file\n5 - Read sudokus from a txt file\n6 - Pick a random sudoku from a file\n'+
         '7 - Exit\nType your option: ')
         if option == '1':
-            gen_9x9_sudoku(True)
+            gen_sudoku(True, NINE_X_NINE)
         elif option == '2':
-            gen_6x6_sudoku()
+            gen_sudoku(True, SIX_X_SIX)
         elif option == '3':
-            gen_4x4_sudoku()
+            gen_sudoku(True, FOUR_X_FOUR)
         elif option == '4':
             number = input('How many sudokus do you want to generate?:')
             dimensions = input('Enter the dimensions of the sudoku [4 to gen a 4x4, 6 to gen a 6x6 or 9 to gen a 9x9]: ') 
